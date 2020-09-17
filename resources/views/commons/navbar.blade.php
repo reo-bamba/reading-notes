ea70ee6181a676b254b59745688f17fe74e78a97
reo bamba <12reo08bam@gmail.com>
2020-09-17 09:30:24 +0000

    top-page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,21 @@
+<header class = "mb-4">
+    <div class = "navbar navbar-expand-sm navbar-dark bg-info">
+        {{-- to top page --}}
+        <a class = "navbar-brand" href = "/">読書notes</a>
+        
+        <button type = "button" class = "navbar-toggler" data-toggle = "collapse" data-target="#nav-bar">
+            <span class = "navbar-toggler-icon"></span>
+        </button>
+        
+        <div class = "collapse navbar-collapse" id = "nav-bar">
+            <ul class = "navbar-nav">
+                {{-- ユーザー登録 --}}
+                <li>ユーザー登録</li>
+                
+                {{-- ユーザーログイン --}}
+                <li>ログイン</li>
+                
+            </ul>
+        </div>
+    </div>
+</header>
