33ccbcdda04c102b5a8c8975f83c8bbada0e413f
reo bamba <12reo08bam@gmail.com>
2020-09-27 04:43:58 +0000

    recorrect

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,7 +60,7 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
-            'region' => 'ap-northeast-1'
+            'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
