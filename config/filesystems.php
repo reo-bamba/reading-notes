4cb9cee8c41f31430f6963621583b3457c25360e
reo bamba <12reo08bam@gmail.com>
2020-09-27 04:40:44 +0000

    s3

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,7 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
+            'region' => 'ap-northeast-1'
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
